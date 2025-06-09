7d168fbe09519d699ccf68be0ecd7b9dd32a2c29
saseoghun <huni032285@gmail.com>
2025-06-09 23:37:24 +0900

    Solving: BOJ3015

---
+++ Problems/BOJ3015.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+
+if n == 2:
+    print(1)
+    sys.exit()
+elif n == 1:
+    print(0)
+    sys.exit()
+
+num = deque()
+num.append(int(sys.stdin.readline().rstrip()))
+
+
+ans = 0
+for i in range(n - 1):
+
+    tmp = int(sys.stdin.readline().rstrip())
+
+    if len(num) == 1:
+        num.append(tmp)
+        continue
+
+    if tmp < num[-1]:
+        cnt = 0
+        while len(num) > 1:
+            cnt += 1
+            num.popleft()
+        ans += sum([i for i in range(1, cnt + 1)])
+        print(f"ans: {ans}")
+    elif tmp == num[-1]:
+
+
+    num.append(tmp)
+
+
+print(ans)
