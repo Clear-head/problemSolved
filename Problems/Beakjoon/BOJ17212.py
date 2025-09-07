5f12cc648eaff2c9b0be86342540222566c67c13
saseoghun <huni032285@gmail.com>
2025-09-07 15:06:47 +0900

    Solved: BOJ17212

--- Problems/Beakjoon/BOJ17212.py
+++ Problems/Beakjoon/BOJ17212.py
@@ -4,7 +4,6 @@ n = int(input())
 
 dp = [0 for _ in range(n+1)]
 for i in range(1, n+1):
-    tmp = [dp[i-j] + 1 for j in coin_weight if i >= j]
-    dp[i] = min(tmp)
+    dp[i] = min([dp[i-j] for j in coin_weight if i >= j]) + 1
 
 print(dp[n])
