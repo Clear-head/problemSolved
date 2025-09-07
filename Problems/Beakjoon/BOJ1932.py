2c59b7efebbc55e70656a6259adc1d7453ea5d64
saseoghun <huni032285@gmail.com>
2025-09-07 17:04:23 +0900

    Solved: BOJ1932

--- Problems/Beakjoon/BOJ1932.py
+++ Problems/Beakjoon/BOJ1932.py
@@ -7,7 +7,7 @@ for _ in range(n):
     triangle.append(list(map(int, stdin.readline().rstrip().split())))
 
 for i in range(n-2, -1, -1):
-    triangle[i] = [max(triangle[i + 1][j] + triangle[i][j], triangle[i + 1][j + 1] + triangle[i][j]) for j in range(len(triangle[i]))]
+    for j in range(len(triangle[i])):
+        triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
 
 print(triangle[0][0])
-
